d2e86f2a6cac97de129b9b5c3d4522edc3e6d679
gpbacay <111699545+gpbacay@users.noreply.github.com>
2023-07-25 05:51:23 +0800

    ...

--- PaLM2_LLM.py
+++ PaLM2_LLM.py
@@ -91,7 +91,7 @@ def run_Bison(reply = reply, messages = messages, context = context, user_name =
         model="models/chat-bison-001",
         context=context,
         messages=messages,
-        temperature=0.1
+        temperature=1.0
     )
     reply = str(reply) + "."
     
