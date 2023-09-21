8e3a6839f3575f255eb62ced0b27e2b585ed1d13
gpbacay <111699545+gpbacay@users.noreply.github.com>
2023-09-21 10:53:28 +0800

    ...

--- haraya_v3.py
+++ haraya_v3.py
@@ -32,7 +32,7 @@ tHeader = Thread(target=print, args=(Header,))
 tHeader.start()
 from PaLM2_LLM import run_Bison
 
-#______________________________________________________VOICE_BOX_PRIMARY_BLOCK/FUNCTION
+#______________________________________________________VOICE_ENGINE_PRIMARY_BLOCK/FUNCTION
 #Run Command: python haraya_v3.py
 recognizer = sr.Recognizer()
 
@@ -46,7 +46,7 @@ def speak(text):
     engine.runAndWait()
     setIsRandom(0)
 
-#______________________________________________________PLAY_A_SOUND_BLOCK/FUNCTION
+#______________________________________________________AUDIO_RESPONSES_BLOCK/FUNCTION
 #Run Command: python haraya_v3.py
 def Play_Prompt_Sound():
     mp3_path = u"audioFiles\\prompt1.mp3"
@@ -64,13 +64,6 @@ def Play_Shutdown_Sound():
 Name = []
 Name_Honorific_Address = []
 NameList = []
-Arithmetic_Addition = []
-Arithmetic_Subtraction = []
-Arithmetic_Multiplication = []
-Arithmetic_Division = []
-Arithmetic_Modulo = []
-Date = []
-count = []
 
 #______________________________________________________FACE_RECOGNITION_BLOCK/FUNCTION
 #Run Command: python haraya_v3.py
