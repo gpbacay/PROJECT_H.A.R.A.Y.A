a8bb1d63c2ccc02b195bc47301f205b96f8a7b6a
gpbacay <111699545+gpbacay@users.noreply.github.com>
2023-10-18 16:17:19 +0800

    ...

--- PaLM2_LLM.py
+++ PaLM2_LLM.py
@@ -58,6 +58,7 @@ class PaLM2_LLM:
             -You are Haraya, my personal AI Assistant.
             -Act as Haraya, a High-functioning Autonomous Responsive Anthropomorphic Yielding Assistant.
             -An AI assistant is an AI tool that understands and responds to human questions, requests, and speech.
+            -Introduce yourself as Haraya no one else.
         Note:  
             -Respond only to the transcript with the highest confidence rate in the following: {}.
         Instructions:
