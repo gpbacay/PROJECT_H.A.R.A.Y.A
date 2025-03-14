d0deee45d7611a0208e6ea94ce2cf2f1b7dbef69
gpbacay <giannebacay2004@gmail.com>
2025-03-14 18:35:45 +0800

    Update attendance record and refactor haraya_v4.py to integrate UserProfile for user management

--- haraya_v4.py
+++ haraya_v4.py
@@ -27,6 +27,7 @@ from pose_recognition_system import Pose_Recognition_System
 from web_data_scraping_system import DataScraper
 from loading_bar import LoadingBar
 from haraya_gui import HarayaUI
+from user_profile import UserProfile
 
 
 class haraya_v4:
@@ -40,10 +41,10 @@ class haraya_v4:
         self.command = "."
         self.ai_command = "."
         self.response = "."
-        self.HonorificAddress = "."
+        self.honorific_address = "."
         
         # Thread 1: AI Agent System
-        self.tAIAgent = Thread(target=HarayaAgent, kwargs={"ai_name": self.getAIName(), "user_name": self.getUserName()}, daemon=True)
+        self.tAIAgent = Thread(target=self.init_agent, daemon=True)
         self.tAIAgent.start()
         
         # Thread 2: GUI System
@@ -59,14 +60,25 @@ class haraya_v4:
         colorama.init(autoreset=True)
         pygame.init()
         self.loading_bar = LoadingBar()
+        self.runLoadingBar = self.loading_bar.run_loadingbar
+        
+        self.user_profile = UserProfile()
+        
         self.tAIAgent.join() # Wait for the AI Agent System to finish
+        
+        self.recognizer = sr.Recognizer()
+        self.engine = pyttsx3.init()
+        self.voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', self.voices[1].id)
+        
+        self.StartUpSequence()
         
      #_________________________________________________________Setters
     # Run Command: python haraya_v4.py
-    def setRunning(self, Running_input: bool):
-        self.running = Running_input
-    def setAIName(self, AIName_input: str):
-        self.ai_name = AIName_input
+    def setRunning(self, running_input: bool):
+        self.running = running_input
+    def setAIName(self, ai_name_input: str):
+        self.ai_name = ai_name_input
     def setCommand(self, command_input: str):
         self.command = command_input
     def setAICommand(self, ai_command_input: str):
@@ -75,8 +87,8 @@ class haraya_v4:
         self.response = response_input
     def setUserName(self, user_name_input: str):
         self.user_name = user_name_input
-    def setHonorificAddress(self, HonorificAddress_input: str):
-        self.HonorificAddress = HonorificAddress_input
+    def setHonorificAddress(self, honorific_address_input: str):
+        self.honorific_address = honorific_address_input
     #_________________________________________________________Getters
     # Run Command: python haraya_v4.py
     def getRunning(self):
@@ -92,20 +104,75 @@ class haraya_v4:
     def getUserName(self):
         return self.user_name
     def getHonorificAddress(self):
-        return self.HonorificAddress
+        return self.honorific_address
     
     #_________________________________________________________Methods
-    def initialize_face_recognition_system(self):
+    # Run Command: python haraya_v4.py
+    def init_agent(self) -> None: 
+        self.agent = HarayaAgent(ai_name=self.getAIName(), user_name=self.getUserName())
+    
+    def initHonorificAddress(self) -> None: # Initialize Honorific Address based on the User's Name
+        Male_Names = ["Gianne Bacay", 
+                    "Earl Jay Tagud",
+                    "Gemmuel Balceda",
+                    "Mark Anthony Lagrosa",
+                    "Klausmieir Villegas",
+                    "CK Zoe Villegas",
+                    "Rolyn Morales",
+                    "Alexander Villasis",
+                    "Bryan Sarpamones"]
+        Female_Names = ["Kleinieir Pearl Kandis Bacay", 
+                        "Princess Viznar", 
+                        "Nichi Bacay",
+                        "Roz Waeschet Bacay", 
+                        "Jane Rose Bandoy"]
+        try:
+            Gender_Name = self.getUserName()
+            if Gender_Name in Male_Names:
+                honorific_address = "Sir"
+            elif Gender_Name in Female_Names:
+                honorific_address = "Ma'am"
+            else:
+                honorific_address = "Boss"
+        except:
+            honorific_address = "Master"
+        finally:
+            self.setHonorificAddress(honorific_address_input=honorific_address)
+            
+    def speak(self, text: str) -> None: # Speak the text using pyttsx3
+        self.isSpeaking(1)
+        self.engine.say(text)
+        self.engine.runAndWait()
+        self.isSpeaking(0)
+        
+    def initialize_face_recognition_system(self) -> None: # Initialize the Face Recognition System
         response = "Initializing Face Recognition System"
+        self.setResponse(response_input=response)
         self.speak(response)
         tFRS = Thread(target=Face_Recognition_System)
         tFRS.start()
         tFRS.join()
         self.runLoadingBar(seconds=0.5, loading_tag="RECOGNIZING FACE...", end_tag="FACE RECOGNIZED!")
-        self.initUserName()
+        self.user_profile.init_user_name()
+        self.setUserName(user_name_input=self.user_profile.get_user_name())
         self.initHonorificAddress()
     
-    
+    def StartUpSequence(self) -> None:
+        self.initialize_face_recognition_system()
+        try:
+            response = self.agent.get_response(question=f"Hi {self.getAIName()}, I am {self.getUserName()}.")
+            print(colorama.Fore.GREEN + response)
+        except Exception as e:
+            print(colorama.Fore.RED + f"An error occurred: {e}")
+            response = f"Hi {self.getHonorificAddress()} {self.getUserName()}, I am {self.getAIName()}, your personal AI assistant! How can I help you?"
+            response1 = (colorama.Fore.GREEN + "Hi " + self.getHonorificAddress() + " " +
+                        colorama.Fore.CYAN + self.getUserName() + colorama.Fore.GREEN +
+                        "! I am Haraya, your personal AI assistant! How can I help you?")
+            print(response1)
+        finally:
+            self.setResponse(response)
+            self.speak(response)
+            return
     
     
     def main(self):
